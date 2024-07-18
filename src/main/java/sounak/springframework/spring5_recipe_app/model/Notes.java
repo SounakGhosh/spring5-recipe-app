a09ea9ef0f091e9c9dfc23f15fd9f8d3f892cb66
sounak <sounak.peter@gmail.com>
2024-07-18 15:32:32 +0530

    One to One JPA Relationship

---
+++ src/main/java/sounak/springframework/spring5_recipe_app/model/Notes.java
@@ -0,0 +1,44 @@
+package sounak.springframework.spring5_recipe_app.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Created by sounak on 18-07-2024.
+ */
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
