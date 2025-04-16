f80a7cad5abc98895b6428973cf7072ff54ba348
sounak <sounak.peter@gmail.com>
2025-04-17 01:38:24 +0530

    Displaying images from Database

--- src/main/java/sounak/springframework/spring5_recipe_app/controllers/ImageController.java
+++ src/main/java/sounak/springframework/spring5_recipe_app/controllers/ImageController.java
@@ -1,6 +1,8 @@
 package sounak.springframework.spring5_recipe_app.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import sounak.springframework.spring5_recipe_app.commands.RecipeCommand;
 import sounak.springframework.spring5_recipe_app.services.ImageService;
 import sounak.springframework.spring5_recipe_app.services.RecipeService;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by sounak on 16-04-2025.
@@ -39,6 +46,24 @@ public class ImageController {
         imageService.saveImageFile(Long.valueOf(id), file);
 
         return "redirect:/recipe/" + id + "/show";
+    }
+
+    @GetMapping("recipe/{id}/recipeImage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+
+            for (Byte wrappedByte : recipeCommand.getImage()){
+                byteArray[i++] = wrappedByte; //auto unboxing
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
     }
 
 }
--- src/test/java/sounak/springframework/spring5_recipe_app/controllers/ImageControllerTest.java
+++ src/test/java/sounak/springframework/spring5_recipe_app/controllers/ImageControllerTest.java
@@ -1,10 +1,12 @@
 package sounak.springframework.spring5_recipe_app.controllers;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -67,6 +69,36 @@ public class ImageControllerTest {
                 .andExpect(MockMvcResultMatchers.header().string("Location", "/recipe/1/show"));
 
         Mockito.verify(imageService, Mockito.times(1)).saveImageFile(Mockito.anyLong(), Mockito.any());
+    }
+
+    @Test
+    public void renderImageFromDB() throws Exception {
+
+        //given
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        String s = "fake image text";
+        Byte[] bytesBoxed = new Byte[s.getBytes().length];
+
+        int i = 0;
+
+        for (byte primByte : s.getBytes()) {
+            bytesBoxed[i++] = primByte;
+        }
+
+        command.setImage(bytesBoxed);
+
+        Mockito.when(recipeService.findCommandById(Mockito.anyLong())).thenReturn(command);
+
+        //when
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/recipeImage"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse();
+
+        byte[] responseBytes = response.getContentAsByteArray();
+
+        Assertions.assertEquals(s.getBytes().length, responseBytes.length);
     }
 
 }
